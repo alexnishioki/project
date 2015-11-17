afa2e565165aeed47abadf2ffea3dccff93f8651
Alex <alexjnishioki@gmail.com>
2015-11-16 16:50:52 -0800

    created canvas element created boundaries on gameboard created static enemy shapes created character shapes

---
+++ firstProject/diff-canvas.js
@@ -0,0 +1,295 @@
+
+    <canvas id="myCanvas" width="1300" height="700"></canvas>
+    <script>
+      var canvas = document.getElementById('myCanvas');
+      var context = canvas.getContext('2d');
+
+      context.beginPath();
+      context.arc(80,25,5,0,2*Math.PI);
+      context.stroke()
+      context.fillStyle = 'red'
+      context.fill()
+
+       context.beginPath();
+      context.moveTo(1,1)
+      context.lineTo(200,200)
+      context.quadraticCurveTo(230,220,310,200)
+      context.lineTo(400,200)
+      context.lineTo(600,200)
+      context.quadraticCurveTo(700,0,310,200)
+      context.fill()
+      context.stroke()
+      context.beginPath()
+      context.moveTo(700,200)
+      context.lineTo(600,200)
+      context.quadraticCurveTo(800,0,420,300)
+      context.stroke()
+      context.fill()
+      context.beginPath()
+      context.moveTo(200,1000)
+      context.lineTo(800,200)
+      context.lineTo(900,800)
+      context.stroke()
+      context.fill()
+      context.beginPath()
+      context.moveTo(20,600)
+      context.lineTo(20,70)
+      context.lineTo(350,400)
+      context.lineTo(300,800)
+      context.fill()
+      context.stroke()
+      context.beginPath()
+      context.moveTo(1500,1)
+      context.lineTo(1000,600)
+      context.lineTo(800,1)
+      context.stroke()
+      context.fill()
+      context.beginPath()
+      context.moveTo(1300,700)
+      context.lineTo(1300,600)
+      context.lineTo(1200,500)
+      context.quadraticCurveTo(1000,0,700,60)
+      context.stroke()
+      context.fill()
+      context.beginPath()
+      context.moveTo(600,400)
+      context.lineTo(410,560)
+      context.lineTo(400,400)
+      context.lineTo(400,600)
+      context.lineTo(600,400)
+      context.fill()
+      context.stroke()
+      context.beginPath()
+      context.moveTo(350,300)
+      context.lineTo(490,400)
+      context.lineWidth = 15
+      context.stroke()
+    </script>
+
+       <canvas id="myCanvas" width="1300" height="700"></canvas>
+
+    <script>
+      var canvas = document.getElementById('myCanvas');
+      var context = canvas.getContext('2d');
+
+
+
+    context.beginPath()
+    context.moveTo(1,1)
+    context.lineTo(1300,20)
+    context.lineTo(1,30)
+    context.stroke()
+    context.fill()
+    context.beginPath()
+    context.moveTo(1200,50)
+    context.lineTo(1,60)
+    context.lineTo(1,90)
+    context.lineTo(1200,50)
+    context.stroke()
+    context.fill()
+    context.beginPath()
+    context.moveTo(60,140)
+    context.lineTo(1300,80)
+    context.lineTo(60,170)
+    context.lineTo(60,140)
+    context.stroke()
+    context.fill()
+    context.beginPath()
+    context.moveTo(1,230)
+    context.lineTo(1200,130)
+    context.lineTo(1,265)
+    context.stroke()
+    context.fill()
+    context.beginPath()
+    context.moveTo(1300,160)
+    context.lineTo(70,320)
+    context.lineTo(1300,200)
+    context.fill()
+    context.stroke()
+    context.beginPath()
+    context.moveTo(1,380)
+    context.lineTo(1200,270)
+    context.lineTo(1,420)
+    context.fill()
+    context.stroke()
+    context.beginPath()
+    context.moveTo(70,500)
+    context.lineTo(1300,320)
+    context.lineTo(70,540)
+    context.lineTo(70,500)
+    context.stroke()
+    context.fill()
+    context.beginPath()
+    context.moveTo(1,620)
+    context.lineTo(1200,400)
+    context.lineTo(1,660)
+    context.fill()
+    context.stroke()
+    context.beginPath()
+    context.moveTo(1,800)
+    context.lineTo(1300,450)
+    context.lineTo(1300,700)
+    context.lineTo(700,1300)
+    context.fill()
+    context.stroke()
+
+    context.beginPath();
+      context.arc(40,45,5,0,2*Math.PI);
+      context.stroke()
+      context.fillStyle = 'blue'
+      context.fill()
+      context.beginPath();
+      context.arc(1250,45,4,0,2*Math.PI);
+      context.stroke()
+      context.fill()
+      context.fillStyle = 'red'
+      context.beginPath();
+      context.arc(41,560,4,0,2*Math.PI);
+      context.stroke()
+      context.fillStyle = 'red'
+      context.fill()
+      context.beginPath();
+      context.arc(37,150,4,0,2*Math.PI);
+      context.stroke()
+      context.fillStyle = 'red'
+      context.fill()
+      context.beginPath();
+      context.arc(1250,130,4,0,2*Math.PI);
+      context.stroke()
+      context.strokeStyle = 'red'
+      context.fill()
+    </script>
+
+    <canvas id="myCanvas" width="1300" height="700"></canvas>
+
+    <script>
+      var canvas = document.getElementById('myCanvas');
+      var context = canvas.getContext('2d');
+
+      context.beginPath();
+      context.arc(10,20,5,0,2*Math.PI);
+      context.stroke()
+      context.fillStyle = 'blue'
+      context.fill()
+
+      context.beginPath();
+      context.arc(1250,40,4,0,2*Math.PI);
+      context.stroke()
+      context.fillStyle = 'red'
+      context.fill()
+
+      context.beginPath();
+      context.arc(1200,150,4,0,2*Math.PI);
+      context.stroke()
+      context.fillStyle = 'red'
+      context.fill()
+
+      context.beginPath()
+      context.moveTo(1,1)
+      context.lineTo(1300,10)
+      context.lineWidth = 20
+      context.stroke()
+      
+      context.beginPath()
+      context.moveTo(1300,10)
+      context.lineTo(1300,700)
+      context.stroke()
+      context.lineWidth = 30
+      
+      context.beginPath()
+      context.moveTo(1200,700)
+      context.lineTo(5,700)
+      context.stroke()
+      context.lineWidth = 30
+      
+      context.beginPath()
+      context.moveTo(1300,10)
+      context.lineTo(1200,700)
+      context.stroke()
+      context.lineWidth = 30
+
+      
+
+      context.beginPath()
+      context.moveTo(5,700)
+      context.lineTo(5,40)
+      context.stroke()
+      context.lineWidth = 30
+
+      context.beginPath()
+      context.moveTo(10,50)
+      context.lineTo(1100,60)
+      context.stroke()
+      context.lineWidth = 30
+
+      context.beginPath()
+      context.moveTo(1100,60)
+      context.lineTo(1150,70)
+      context.stroke()
+
+      
+      context.beginPath()
+      context.moveTo(1100,70)
+      context.lineTo(1200,80)
+      context.stroke()
+
+      context.beginPath()
+      context.moveTo(1200,80)
+      context.lineTo(1050,90)
+      context.stroke()
+
+      context.beginPath()
+      context.moveTo(100,150)
+      context.lineTo(1000,130)
+      context.stroke()
+      context.beginPath()
+      context.moveTo(1030,140)
+      context.lineTo(1020,140)
+      context.stroke()
+
+      context.beginPath()
+      context.moveTo(50,100)
+      context.lineTo(50,700)
+      context.stroke()
+      context.lineWidth = 40
+
+      context.beginPath()
+      context.moveTo(55,100)
+      context.lineTo(150,700)
+      context.stroke()
+      context.lineWidth = 20
+
+      context.beginPath()
+      context.moveTo(1220,150)
+      context.lineTo(1150,600)
+      context.stroke()
+      context.lineWidth = 20
+      context.beginPath()
+      context.moveTo(1220,150)
+      context.lineTo(700,220)
+      context.stroke()
+      context.lineWidth = 25
+
+      context.beginPath()
+      context.moveTo(1165,200)
+      context.lineTo(900,500)
+      context.stroke()
+      context.lineWidth = 20
+
+      context.beginPath()
+      context.moveTo(100,200)
+      context.lineTo(300,600)
+      context.stroke()
+      context.lineWidth = 20
+
+      context.beginPath()
+      context.moveTo(150,200)
+      context.lineTo(380,640)
+      context.stroke()
+      context.lineWidth = 20
+
+
+      
+
+      
+    </script>
