1c6893dc48bb0ece42777bc60eeb90829568c9cc
Alex <alexjnishioki@gmail.com>
2015-11-17 16:46:07 -0800

    new board

---
+++ firstProject/javascript/board.js
@@ -0,0 +1,11 @@
+var Board = function (entities) {
+this.entities = entities
+this.canvas = document.createElement("canvas")
+this.canvas.height = 700
+this.canvas.width = 1300
+}
+Board.prototype.draw = function() {
+for(var i = 0; i < this.entities.length; i++) {
+this.entities[i].draw(this.canvas)
+}
+}
