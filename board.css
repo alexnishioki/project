aa04e65afa253314847cdc9b8906ceed426bfac8
Alex <alexjnishioki@gmail.com>
2015-12-01 13:16:59 -0800

    moving files up one dir

---
+++ board.css
@@ -0,0 +1,23 @@
+body {
+	background:linear-gradient(bottom right,#E1E1E2,#FFFFFF) ;
+}
+#timer{
+	position:fixed;
+	top:10px;
+	left:1230px;
+	color:black;
+	font-family:Impact;
+	font-size:20px;
+	opacity:.5;
+}
+
+#help{
+	position:fixed;
+	font-size:12px;
+	left:1216px;
+	top:612px;
+	font-family:Monaco;
+	opacity:.5;
+	color:;
+
+}
